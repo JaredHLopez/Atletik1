5ec5f6cfd616bf1888e80188bb1eb2f5ced1f33b
Elliot Halili <elliot.jacob09@gmail.com>
2025-10-07 11:46:25 +0800

    added color to hero

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -57,29 +57,32 @@ function Home() {
           SECTION 1: HERO
       ============================== */}
       <section className="hero-section">
-        <img src="/home-feed.png" alt="Atletik Logo" className="appImage" />
-        {/*<h1 className="heading">Atletik</h1>*/}
-        {/*<h2 className="sub-heading">Welcome to Atletik</h2>*/}
+        <div id="color-bomb" />
+        <div id={'frosted-glass'} />
+        <div id="hero-content-container">
+          <img src="/home-feed.png" alt="Atletik Logo" className="appImage" />
+          {/*<h1 className="heading">Atletik</h1>*/}
+          {/*<h2 className="sub-heading">Welcome to Atletik</h2>*/}
+          <p className="text hero-text">
+            Discover and join sports events near you with <span className="logo-callout">Atletik</span>. Connect with teams, clubs, and players in your area!
+          </p>
 
-        <p className="text hero-text">
-          Discover and join sports events near you with <span className="logo-callout">Atletik</span>. Connect with teams, clubs, and players in your area!
-        </p>
-
-        <a
-            href="https://drive.google.com/uc?export=download&id=1vtuJpwteirRY7kn3ATdSP2DGoiIjkdAT"
-            className="download-button"
-            download
-        >
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-               stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-               className="icon icon-tabler icons-tabler-outline icon-tabler-download">
-            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-            <path d="M4 17v2a2 2 0 0 0 2 2h12a2 2 0 0 0 2 -2v-2"/>
-            <path d="M7 11l5 5l5 -5"/>
-            <path d="M12 4l0 12"/>
-          </svg>
-          Download APK
-        </a>
+          <a
+              href="https://drive.google.com/uc?export=download&id=1vtuJpwteirRY7kn3ATdSP2DGoiIjkdAT"
+              className="download-button"
+              download
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                 stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                 className="icon icon-tabler icons-tabler-outline icon-tabler-download">
+              <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+              <path d="M4 17v2a2 2 0 0 0 2 2h12a2 2 0 0 0 2 -2v-2"/>
+              <path d="M7 11l5 5l5 -5"/>
+              <path d="M12 4l0 12"/>
+            </svg>
+            Download APK
+          </a>
+        </div>
       </section>
 
       {/* ==============================
